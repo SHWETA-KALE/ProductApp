1bc2de728e9c63ed820bd62dfbc1255934c97af9
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-12 19:26:50 +0530

    ProductApp using getters and setters method

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.models;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Prod1 = new Product();
+            Product Prod2 = new Product();
+
+            Prod1.SetId(1);
+            Prod1.SetName("Computer");
+            Prod1.SetPrice(50000);
+            Prod1.SetDiscountPercentage(20);
+
+            Prod2.SetId(2);
+            Prod2.SetName("Laptop");
+            Prod2.SetPrice(40000);
+            Prod2.SetDiscountPercentage(30);
+
+
+            // Displaying details and cost after discount
+            Console.WriteLine($"Product ID: {Prod1.GetId()}\n" +
+                $"Name: {Prod1.GetName()}\n" +
+                $"Price: {Prod1.GetPrice()} \n" +
+                $"Discount: {Prod1.GetDiscountPercentage()} \n" +
+                $"CostAfterDiscount: {Prod1.CostAfterDiscount()} \n");
+
+            //Console.WriteLine($"Product ID: {Prod2.GetId()}, Name: {Prod2.GetName()}, Price: {Prod2.GetPrice()}, Discount: {Prod2.GetDiscountPercentage()}%, Cost after discount: {Prod2.CostAfterDiscount()}"));
+            Console.WriteLine($"Product ID: {Prod2.GetId()}\n" +
+                $"Name: {Prod2.GetName()}\n" +
+                $"Price: {Prod2.GetPrice()}\n" +
+                $"Discount: {Prod2.GetDiscountPercentage()}\n" +
+                $"CostAfterDiscount: {Prod2.CostAfterDiscount()}\n");
+                
+
+
+
+        }
+    }
+}
---
+++ models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.models
+{
+    internal class Product
+    {
+        int _id;
+        string _name;
+        double _price;
+        double _discountPercentage;
+
+        //getters method
+        public int GetId() { return _id; }
+        public string GetName() { return _name; }
+
+        public double GetPrice() { return _price; }
+        public double GetDiscountPercentage() { return _discountPercentage; }
+
+        //setters method
+        public void SetId(int id) { _id = id; }
+        public void SetName(string name) { _name = name; }
+
+        public void SetPrice(double price) { _price = price; }
+        public void SetDiscountPercentage(double discountPercentage) { _discountPercentage = discountPercentage; }
+
+
+        public double CostAfterDiscount()
+        {
+            return _price * (1 - _discountPercentage/100);
+        }
+    }
+}
+
+
